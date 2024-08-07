3977db838de9d165638430d9cf60103f0fb3f053
Rhuan Nascimento <rhuan2414@gmail.com>
2024-08-07 20:57:43 -0300

    CRUD FUNCIONARIO

--- src/main/java/com/biblioteca/scbapi/api/controller/FuncionarioController.java
+++ src/main/java/com/biblioteca/scbapi/api/controller/FuncionarioController.java
@@ -1,15 +1,17 @@
 package com.biblioteca.scbapi.api.controller;
 
 import com.biblioteca.scbapi.api.dto.FuncionarioDTO;
+import com.biblioteca.scbapi.exception.RegraNegocioException;
 import com.biblioteca.scbapi.model.entity.Funcionario;
 import com.biblioteca.scbapi.service.FuncionarioService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -23,4 +25,61 @@ public class FuncionarioController {
         List<Funcionario> alunos = service.getFuncionarios();
         return ResponseEntity.ok(alunos.stream().map(FuncionarioDTO::create).collect(Collectors.toList()));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id) {
+        Optional<Funcionario> funcionario = service.getFuncionarioById(id);
+        if (!funcionario.isPresent()) {
+            return new ResponseEntity("Funcionário não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(funcionario.map(FuncionarioDTO::create));
+    }
+
+    @PostMapping()
+    public ResponseEntity post(@RequestBody FuncionarioDTO dto) {
+        try {
+            Funcionario funcionario = converter(dto);
+            funcionario = service.salvar(funcionario);
+            return new ResponseEntity(funcionario, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        Optional<Funcionario> funcionario = service.getFuncionarioById(id);
+        if (!funcionario.isPresent()) {
+            return new ResponseEntity("Funcionario não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(funcionario.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody FuncionarioDTO dto) {
+        if (!service.getFuncionarioById(id).isPresent()) {
+            return new ResponseEntity("Funcionario não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            Funcionario funcionario = converter(dto);
+            funcionario.setId(id);
+            service.salvar(funcionario);
+            return ResponseEntity.ok(funcionario);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public Funcionario converter(FuncionarioDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Funcionario funcionario = modelMapper.map(dto, Funcionario.class);
+
+        return funcionario;
+    }
+
 }
--- src/main/java/com/biblioteca/scbapi/service/FuncionarioService.java
+++ src/main/java/com/biblioteca/scbapi/service/FuncionarioService.java
@@ -1,11 +1,14 @@
 package com.biblioteca.scbapi.service;
 
 
+import com.biblioteca.scbapi.exception.RegraNegocioException;
 import com.biblioteca.scbapi.model.entity.Funcionario;
 import com.biblioteca.scbapi.model.repository.FuncionarioRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 
@@ -24,4 +27,35 @@ public class FuncionarioService {
     public Optional<Funcionario> getFuncionarioById(Long id) {
         return repository.findById(id);
     }
+
+    @Transactional
+    public Funcionario salvar(Funcionario funcionario) {
+        validar(funcionario);
+        return repository.save(funcionario);
+    }
+
+    @Transactional
+    public void excluir(Funcionario funcionario) {
+        Objects.requireNonNull(funcionario.getId());
+        repository.delete(funcionario);
+    }
+
+    public void validar(Funcionario funcionario) {
+        if (funcionario.getMatricula() == null || funcionario.getMatricula().length() != 8 || funcionario.getMatricula().contains(" ")) {
+            throw new RegraNegocioException("Matrícula inválida");
+        }
+        if (funcionario.getNome() == null || funcionario.getNome().trim().equals("")) {
+            throw new RegraNegocioException("Nome inválido");
+        }
+        if (funcionario.getCpf() == null || funcionario.getCpf().trim().equals("")) {
+            throw new RegraNegocioException("CPF inválido");
+        }
+        if (funcionario.getEmail() == null || funcionario.getEmail().trim().equals("")) {
+            throw new RegraNegocioException("Email inválido");
+        }
+        if (funcionario.getTelefone() == null || funcionario.getTelefone().trim().equals("")) {
+            throw new RegraNegocioException("Telefone inválido");
+        }
+    }
+
 }
